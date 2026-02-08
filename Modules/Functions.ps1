f618e70df0b80b4331d3c2cb68e8e67a7cd7ec0d
Pinak Dhabu <thepinak503@duck.com>
2026-02-09 00:14:22 +0530

    Refactor: Move all aliases from Aliases.ps1 to Functions.ps1 with proper @args syntax
    - Moved 81 functions from Aliases.ps1 to Functions.ps1
    - Fixed all functions to use proper PowerShell @args parameter instead of
    - Consolidated Git, Docker, Kubernetes, Python, Node.js, Rust, Terraform, and AWS functions
    - Removed duplicate and incorrectly formatted function definitions
    - Ensured consistent function syntax across all development tool aliases

--- Modules/Functions.ps1
+++ Modules/Functions.ps1
@@ -320,6 +320,211 @@ function weather {
 }
 #endregion
 
+#region Git
+function g { git @args }
+function ga { git add @args }
+function gaa { git add --all @args }
+function gb { git branch @args }
+function gba { git branch -a @args }
+function gc { git commit @args }
+function gcm { git commit -m @args }
+function gcam { git commit -am @args }
+function gco { git checkout @args }
+function gcb { git checkout -b @args }
+function gd { git diff @args }
+function gds { git diff --staged @args }
+function gf { git fetch @args }
+function gl { git log --oneline --graph --decorate @args }
+function glog { git log --oneline --graph --decorate --all @args }
+function gm { git merge @args }
+function gp { git push @args }
+function gpf { git push --force-with-lease @args }
+function gpl { git pull @args }
+function gr { git remote -v @args }
+function grb { git rebase @args }
+function grbi { git rebase -i @args }
+function gs { git status -sb @args }
+function gst { git stash @args }
+function gstp { git stash pop @args }
+function gsta { git stash apply @args }
+function gstl { git stash list @args }
+function gcl { git clone @args }
+function gcp { git cherry-pick @args }
+function grm { git rm @args }
+function grmc { git rm --cached @args }
+function gundo { git reset HEAD~1 --mixed }
+function gclean { git clean -fd }
+function gpristine { git reset --hard; git clean -dfx }
+function gwip { git add -A; git commit -m "--wip-- skip ci" }
+function gunwip { git log -n 1 | Select-String "--wip--" && git reset HEAD~1 }
+function lazyg { 
+    param([string]$Message) 
+    if (-not $Message) { 
+        Write-Host "Usage: lazyg <message>" -ForegroundColor Red; 
+        return 
+    } 
+    git add .; git commit -m "$Message"; git push 
+}
+function gcurrent { git branch --show-current }
+function gdefault { git symbolic-ref refs/remotes/origin/HEAD --short | ForEach-Object { $_ -replace "origin/", "" } }
+function gstats { git shortlog -sn }
+function gbd { git branch -d @args }
+function gbD { git branch -D @args }
+function gbm { git branch -m @args }
+#endregion
+
+#region Docker
+function d { docker @args }
+function dc { docker-compose @args }
+function dps { Get-DockerContainers }
+function dpa { Get-DockerAllContainers }
+function di { Get-DockerImages }
+function dl { Get-DockerLogs @args }
+function dex { 
+    param([string]$Container) 
+    docker exec -it $Container sh 
+}
+function dr { 
+    param([string]$Image) 
+    docker run -it --rm $Image 
+}
+function dri { 
+    param([string]$Image) 
+    docker run -it $Image 
+}
+function dprune { docker system prune -af }
+function dprunev { docker volume prune -f }
+function dstats { docker stats --format "table {{.Name}}\t{{.CPUPerc}}\t{{.MemUsage}}\t{{.NetIO}}" }
+function dcu { docker-compose up @args }
+function dcud { docker-compose up -d @args }
+function dcd { docker-compose down @args }
+function dcdv { docker-compose down -v @args }
+function dcr { docker-compose restart @args }
+function dcb { docker-compose build @args }
+function dcl { docker-compose logs -f @args }
+function dce { docker-compose exec @args }
+function dcs { docker-compose stop @args }
+function dcstart { docker-compose start @args }
+#endregion
+
+#region Kubernetes
+function k { kubectl @args }
+function kg { Get-Kubectl @args }
+function kd { Describe-Kubectl @args }
+function kl { Logs-Kubectl @args }
+function kex { kubectl exec -it @args }
+function kgp { kubectl get pods @args }
+function kgs { kubectl get svc @args }
+function kgd { kubectl get deployment @args }
+function kgn { kubectl get nodes @args }
+function kgns { kubectl get namespace @args }
+function kga { kubectl get all @args }
+function kgaa { kubectl get all --all-namespaces @args }
+function kdp { kubectl describe pod @args }
+function kds { kubectl describe svc @args }
+function kdd { kubectl describe deployment @args }
+function kdn { kubectl describe node @args }
+function kpf { kubectl port-forward @args }
+function ktop { kubectl top @args }
+function ka { kubectl apply @args }
+function kdel { kubectl delete @args }
+function h { helm @args }
+function hin { helm install @args }
+function hup { helm upgrade @args }
+function hdel { helm delete @args }
+function hls { helm list @args }
+function hsearch { helm search hub @args }
+#endregion
+
+#region Python
+function py { python @args }
+function py3 { python3 @args }
+function pip { pip3 @args }
+function pipi { pip install @args }
+function pipu { pip install --upgrade @args }
+function pipun { pip uninstall @args }
+function pipl { pip list @args }
+function pipf { pip freeze @args }
+function pipo { pip list --outdated @args }
+function venv { python -m venv @args }
+function venva { .\venv\Scripts\Activate.ps1 }
+function venvd { deactivate }
+function po { poetry @args }
+function poa { poetry add @args }
+function pou { poetry update @args }
+function poi { poetry install @args }
+function por { poetry run @args }
+function pos { poetry shell @args }
+function pob { poetry build @args }
+function popub { poetry publish @args }
+#endregion
+
+#region Node.js
+function nr { npm run @args }
+function ns { npm start @args }
+function nb { npm run build @args }
+function nt { npm test @args }
+function ni { npm install @args }
+function nid { npm install --save-dev @args }
+function nig { npm install -g @args }
+function nu { npm uninstall @args }
+function nup { npm update @args }
+function nls { npm list --depth=0 @args }
+function nout { npm outdated @args }
+function nci { npm ci @args }
+function yr { yarn run @args }
+function ys { yarn start @args }
+function yb { yarn build @args }
+function yt { yarn test @args }
+function ya { yarn add @args }
+function yad { yarn add --dev @args }
+function yrm { yarn remove @args }
+function yu { yarn upgrade @args }
+function pn { pnpm @args }
+function pni { pnpm install @args }
+function pnr { pnpm run @args }
+function pns { pnpm start @args }
+#endregion
+
+#region Rust
+function c { cargo @args }
+function cb { cargo build @args }
+function cbr { cargo build --release @args }
+function cr { cargo run @args }
+function ct { cargo test @args }
+function cc { cargo check @args }
+function cf { cargo fmt @args }
+function clippy { cargo clippy @args }
+function cdoc { cargo doc --open @args }
+function cnew { cargo new @args }
+function cinit { cargo init @args }
+function cpub { cargo publish @args }
+function gob { go build @args }
+function gor { go run @args }
+function got { go test @args }
+function goi { go install @args }
+function gog { go get @args }
+#endregion
+
+#region Terraform
+function tf { terraform @args }
+function tfa { terraform apply @args }
+function tfauto { terraform apply -auto-approve @args }
+function tfd { terraform destroy @args }
+function tff { terraform fmt @args }
+function tfi { terraform init @args }
+function tfp { terraform plan @args }
+function tfv { terraform validate @args }
+function tfw { terraform workspace @args }
+#endregion
+
+#region AWS
+function awsls { aws s3 ls @args }
+function awscp { aws s3 cp @args }
+function awssync { aws s3 sync @args }
+function awswho { aws sts get-caller-identity @args }
+#endregion
+
 #region Process Management
 function psgrep {
     <#
@@ -619,6 +824,13 @@ function emptybin {
         Write-Host "Linux trash emptying not implemented" -ForegroundColor Yellow
     }
 }
+#endregion
+
+#region AWS
+function awsls { aws s3 ls @args }
+function awscp { aws s3 cp @args }
+function awssync { aws s3 sync @args }
+function awswho { aws sts get-caller-identity @args }
 #endregion
 
 #region Quick Access
